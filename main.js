45c1616cb64e73f8848eb21e2fa5f5246d19f72a
Jean Honlet <jehon@users.noreply.github.com>
2022-07-18 11:56:40 +0000

    chore: first main skeleton

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
 
 // Thanks to https://github.com/iFraan/tmdb.js
+
+import yargs from 'yargs';
+
+const options = await yargs(process.argv.slice(2))
+    .options({
+        'dryRun': {
+            alias: ['dry-run', 'n'],
+            type: 'boolean',
+            default: false,
+            coerce: (val) => {
+                if (val) {
+                    console.info('Using dry run mode');
+                }
+                return val;
+            }
+        },
+        'verbose': {
+            alias: ['v'],
+            type: 'boolean',
+            default: false
+        },
+        'file': {
+            // Also posible at end : $0 ... <file>
+            alias: ['f'],
+            type: 'string',
+            default: ''
+        }
+    })
+    .command('$0 <file>', 'The initial movie')
+    .recommendCommands()
+    .strict()
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// console.log(options);
+
+const f = options.file;
+
+process.stdout.write(`Looking for ${f}...`);
+
+process.stdout.write('\n');
+
