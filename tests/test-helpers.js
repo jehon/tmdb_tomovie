e224f7e5ed21e00e52d7a21253b465bcb2f67f94
Jean Honlet <jehon@users.noreply.github.com>
2022-07-18 11:49:41 +0000

    test: create skeleton

---
+++ tests/test-helpers.js
@@ -0,0 +1,36 @@
+
+import path, { dirname } from 'path';
+
+/**
+ * @param {object|string} meta from import.meta
+ * @returns {string} the test name
+ */
+export function t(meta) {
+    const url = (typeof (meta) == 'object' && 'url' in meta) ? meta.url : meta;
+    return new URL(url).pathname.split('/').pop();
+}
+
+/**
+ * @param {*} meta the meta object from import.meta
+ * @returns {string} the pathname
+ */
+export function __filename(meta) {
+    const url = (typeof (meta) == 'object' && 'url' in meta) ? meta.url : meta;
+    return new URL(url).pathname;
+}
+
+/**
+ * @param {*} meta the meta object from import.meta
+ * @returns {string} the dirname
+ */
+export function __dirname(meta) {
+    const url = (typeof (meta) == 'object' && 'url' in meta) ? meta.url : meta;
+    return dirname(new URL(url).pathname);
+}
+
+export const fromCWD = (...args) => path.join(process.cwd(), ...args);
+export const rootPath = (...args) => path.join((path.dirname(__dirname(import.meta))), ...args);
+
+// Test
+export const dataPath = (...args) => rootPath('test', 'data', ...args);
+export const tempPath = (...args) => rootPath('tmp', 'unit', ...args);
---
+++ tests/unit/basic-test.mjs
@@ -0,0 +1,8 @@
+
+import { t } from '../test-helpers.js';
+
+describe(t(import.meta), () => {
+    it('should be true', () => {
+        expect(true).toBeTruthy();
+    });
+});
