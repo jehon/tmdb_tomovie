8e80f7ea88104278b2ee60a2836883f51adcd215
Jean Honlet <jehon@users.noreply.github.com>
2022-10-11 18:12:26 +0000

    fix: reduce log of create

--- tests/create.sh
+++ tests/create.sh
@@ -5,13 +5,16 @@ set -o errexit
 # shellcheck source=/usr/bin/jh-lib
 . jh-lib
 
-TMP="$( realpath "$JH_SWD"/../tmp )"
-mkdir -p "$TMP"
+TARGET="${1:-$JH_SWD/tmp/test.mkv}"
+mkdir -p "$( dirname "$TARGET" )"
+
+echo "Generating $TARGET"
 
 # https://superuser.com/a/1003429/287025
-
-ffmpeg -y -f lavfi \
+ffmpeg \
+    -hide_banner -loglevel error \
+    -y -f lavfi \
     -i anullsrc -loop 1 \
     -f image2 -i "$JH_SWD"/create.jpg \
     -r 30 -t 10 -pix_fmt yuvj420p -map 0:a -map 1:v \
-    "$TMP"/video.mkv
+    "$TARGET"
