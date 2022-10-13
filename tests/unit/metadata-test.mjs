1790afaacf67b5f32e3bad0c9e84ea4a0dbe7409
Jean Honlet <jehon@users.noreply.github.com>
2022-10-13 14:37:01 +0000

    refactor: rename to metadata

--- tests/unit/metadata-test.mjs
+++ tests/unit/metadata-test.mjs
@@ -1,5 +1,5 @@
 
-import Infos from '../../lib/infos.js';
+import Metadata from '../../lib/metadata.js';
 import { getFileSize, loadJSONData, t, tempPath } from '../test-helpers.js';
 
 describe(t(import.meta), () => {
@@ -7,7 +7,7 @@ describe(t(import.meta), () => {
     it('should get poster', async () => {
         const target = tempPath('infos_ghost_1990.jpg');
 
-        const infos = Infos.fromTMDB(json);
+        const infos = Metadata.fromTMDB(json);
         await infos.savePosterTo(target);
 
         expect(getFileSize(target)).toBeGreaterThan(50);
