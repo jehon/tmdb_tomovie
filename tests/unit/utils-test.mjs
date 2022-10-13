8e47e4c86e51e7ea2069a471ebb5b7c73646d372
Jean Honlet <jehon@users.noreply.github.com>
2022-10-13 14:30:25 +0000

    test: add more tests

--- tests/unit/utils-test.mjs
+++ tests/unit/utils-test.mjs
@@ -5,19 +5,33 @@ import fs from 'fs';
 
 describe(t(import.meta), () => {
     describe('parseFilename', () => {
-        const tfn = (fn, title, year) => it(fn, () => {
+        const tfn = (fn, title, year, cb = () => { }) => it(fn, () => {
             const parsed = parseFilename(fn);
+            expect(parsed.path)
+                .withContext(`path of ${fn}`)
+                .toBe(fn);
             expect(parsed.title)
                 .withContext(`title of ${fn}`)
                 .toBe(title);
             expect(parsed.year)
                 .withContext(`year of ${fn}`)
                 .toBe(year);
+
+            cb(parsed);
         });
 
 
 
         tfn('Ghost (1990)', 'Ghost', '1990');
+
+        tfn('Ghost (1990).mkv', 'Ghost', '1990', (parsed) => {
+            expect(parsed.basepath).toBe('./Ghost (1990)');
+        });
+
+        tfn('/some/where/Ghost (1990).mkv', 'Ghost', '1990', (parsed) => {
+            expect(parsed.extension).toBe('.mkv');
+            expect(parsed.basepath).toBe('/some/where/Ghost (1990)');
+        });
     });
 
     describe('getBinary', () => {
